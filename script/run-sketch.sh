7cb8c80ec4d225ac13c597d68ab79c4668f00646
sbarman <sbarman@L3CWZ5T.(none)>
2011-05-22 20:28:03 -1000

    Updated script to limit memory

--- script/run-sketch.sh
+++ script/run-sketch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+ulimit -d 100000 -m 100000 -v 1000000
 #perl -n -e '/^\{\"input\"\=\>\"(.*)\"\}$/ && print "$1\n"' temp.sk > temp1.sk
 sed 's/\\r//g' $1 > sketchtemp/temp2.sk
 sed 's/\\n//g' sketchtemp/temp2.sk > sketchtemp/temp3.sk
 sed 's/\\t//g' sketchtemp/temp3.sk > sketchtemp/temp4.sk
-sketch sketchtemp/temp4.sk > sketchtemp/sketchoutput
+sketch --slv-timeout=1 sketchtemp/temp4.sk > sketchtemp/sketchoutput
 script/sketch2html.sh sketchtemp/sketchoutput

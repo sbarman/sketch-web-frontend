0e501928c427d44b050743a8208bb7c17032ec44
sbarman <sbarman@L3CWZ5T.(none)>
2011-06-15 16:38:31 -0700

    added formatting script, removed black background

--- script/run-sketch.sh
+++ script/run-sketch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ulimit -d 100000 -m 100000 -v 1000000
+ulimit -d 200000 -m 200000 -v 4000000
 #perl -n -e '/^\{\"input\"\=\>\"(.*)\"\}$/ && print "$1\n"' temp.sk > temp1.sk
-sed 's/\\r//g' $1 > sketchtemp/temp2.sk
+cat $1  | sed 's/\$\}/\$\}\n/g' |  awk -f script/expand.awk > sketchtemp/temp1.sk; 
+sed 's/\\r//g' sketchtemp/temp1.sk > sketchtemp/temp2.sk
 sed 's/\\n//g' sketchtemp/temp2.sk > sketchtemp/temp3.sk
 sed 's/\\t//g' sketchtemp/temp3.sk > sketchtemp/temp4.sk
 sketch --slv-timeout=1 sketchtemp/temp4.sk > sketchtemp/sketchoutput
